73715a6541bc48659f239e407bc30f662bf63a92
Alexander Nikolskiy <vsol@bk.ru>
2017-02-18 14:37:57 +0300

    Test upload

---
+++ assets/js/app.js
@@ -0,0 +1,9 @@
+(function() {
+  'use strict';
+
+  // setTimeout(function() {
+  //   document.querySelector('.greating_picture').classList.add('m--show');
+  // }, 1000);
+  svg4everybody();
+})();
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImFwcC5qcyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFBQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBIiwiZmlsZSI6ImFwcC5qcyIsInNvdXJjZXNDb250ZW50IjpbIihmdW5jdGlvbigpIHtcclxuICAndXNlIHN0cmljdCc7XHJcblxyXG4gIC8vIHNldFRpbWVvdXQoZnVuY3Rpb24oKSB7XHJcbiAgLy8gICBkb2N1bWVudC5xdWVyeVNlbGVjdG9yKCcuZ3JlYXRpbmdfcGljdHVyZScpLmNsYXNzTGlzdC5hZGQoJ20tLXNob3cnKTtcclxuICAvLyB9LCAxMDAwKTtcclxuICBzdmc0ZXZlcnlib2R5KCk7XHJcbn0pKCk7Il19
